dc34ff840b9d19dd91aa468f51f3bdc2c9fd44bc
judah <judahlibera@gmail.com>
2021-11-20 13:10:24 -0500

    Initial commit

---
+++ header.h
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Judah Libera
+ */
+
+#ifndef UNTITLED_HEADER_H
+#define UNTITLED_HEADER_H
+
+#include <iostream>
+#include <string>
+
+#endif //UNTITLED_HEADER_H
---
+++ main.cpp
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Judah Libera
+ */
+
+#include "header.h";
+
+using namespace std;
+int main() {
+    string str;
+    cout << "enter operation and two operands." << endl;
+    getline(cin, str);
+    string op = str.substr(0, str.find(" "));
+    str.erase(0, str.find(" ") + 1);
+    string num = str.substr(0, str.find(" "));
+    str.erase(0, str.find(" ") + 1);
+    string num2 = str.substr(0, str.find(" "));
+
+    double res;
+    if(op == "+")
+        res = stof(num) + stof(num2);
+    else if(op == "-")
+        res = stof(num) - stof(num2);
+    else if(op == "*")
+        res = stof(num) * stof(num2);
+    else if(op == "/")
+        res = stof(num) / stof(num2);
+    else
+        cout << "invalid operator" << endl;
+
+    cout << res << endl;
+
+    return 0;
+}
